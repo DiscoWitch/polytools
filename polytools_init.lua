7ca53b5f1e97d154041d71752829646f0f045301
discowitch <disco.witchcraft@gmail.com>
2020-11-29 14:10:34 -0500

    Fix polytools init error.

--- polytools_init.lua
+++ polytools_init.lua
@@ -5,12 +5,29 @@ local polytools = {}
 function polytools.init(polytools_path)
     polytools_path = polytools_path .. "/"
     polytools_path = string.gsub(polytools_path, "/+", "/")
-    local mod_files = {"apply.lua", "delay_load.lua", "depoly_death.lua", "effect_silent.xml",
-                       "effect.xml", "fix_physics.lua", "fix_projectiles.lua", "null.xml",
-                       "polytools.lua", "ui/heart.lua", "ui/heart.xml", "ui/nugget.xml",
-                       "ui/on_remove.lua", "ui/player_base_hide.xml", "ui/player_base_worm.xml",
-                       "ui/player_base.xml", "ui/polywand_switch.lua", "ui/polywand_throw.lua",
-                       "ui/polywand.lua", "ui/polywand.xml"}
+    local mod_files = {
+        "apply.lua",
+        "delay_load.lua",
+        "depoly_death.lua",
+        "effect_silent.xml",
+        "effect.xml",
+        "fix_physics.lua",
+        "fix_explode_death.lua",
+        "fix_explode_lifetime.lua",
+        "null.xml",
+        "polytools.lua",
+        "ui/heart.lua",
+        "ui/heart.xml",
+        "ui/nugget.xml",
+        "ui/on_remove.lua",
+        "ui/player_base_hide.xml",
+        "ui/player_base_worm.xml",
+        "ui/player_base.xml",
+        "ui/polywand_switch.lua",
+        "ui/polywand_throw.lua",
+        "ui/polywand.lua",
+        "ui/polywand.xml"
+    }
     for k, v in ipairs(mod_files) do
         local file_path = polytools_path .. v
         local text = ModTextFileGetContent(file_path)
